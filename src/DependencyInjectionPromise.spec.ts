758725ea52c98cd90f96e074c2452689e97719ae
Staffan Olsson <staffan@repos.se>
2018-03-25 16:06:39 +0200

    Spec compliant! Gave up on overriding promise with a no-arg constructor.

--- src/DependencyInjectionPromise.spec.ts
+++ src/DependencyInjectionPromise.spec.ts
@@ -1,6 +1,30 @@
-import DependencyInjectionPromise from './DependencyInjectionPromise';
+import { Dependency, DependencyLazy, DependencyPromise } from './DependencyInjectionPromise';
 
-describe("DependencyInjectionPromise", () => {
+describe("DependencyPromise", () => {
+
+  it("Is merely a regular promise", done => {
+    let dependency = new DependencyPromise<number>((resolve, reject) => {
+      setTimeout(resolve.bind(null, 123), 1);
+    });
+    dependency.then(value => {
+      expect(value).toBe(123);
+      done();
+    });
+  });
+
+  it("Has a setProvider method that always throws", () => {
+    let dependency = new DependencyPromise<number>((resolve, reject) => {});
+    try {
+      dependency.setProvider((resolve, reject) => {});
+      fail("Should have thrown when trying to set a provider to a DependencyPromise");
+    } catch (e) {
+      expect(e.message).toMatch(/no need for a provider/);
+    }
+  });
+
+});
+
+describe("DependencyInjectionLazy", () => {
 
   xit("The only type of dependency injection we do is lazy singleton");
 
@@ -8,23 +32,38 @@ describe("DependencyInjectionPromise", () => {
 
   describe("Promise-like API", () => {
 
-    it("Takes no constructor argument, so that a promise+type can be declared synchronously in config", () => {
-      new DependencyInjectionPromise<number>();
+    it("Takes no constructor argument, so that the promise can be declared synchronously in config", () => {
+      new DependencyLazy<number>();
     });
 
   });
 
-  describe(".providerRegister", () => {
+  describe(".setProvider", () => {
 
-    it("Expects an executor that can be invoked on first .then", () => {
-      let promise = new DependencyInjectionPromise<number>();
-      /*promise.providerRegister((resolve, reject) => {
-
-      });*/
+    it("Expects an executor that can be invoked on first .then", done => {
+      let promise = new DependencyLazy<number>();
+      let executorCalled = false;
+      promise.setProvider((resolve, reject) => {
+        executorCalled = true;
+        setTimeout(resolve.bind(null, 234), 10);
+      });
+      expect(executorCalled).toBeFalsy();
+      promise.then(value => {
+        expect(value).toBe(234);
+        done();
+      });
+      expect(executorCalled).toBeTruthy();
     });
 
     it("Throws if there is already a provider", () => {
-
+      let lazy = new DependencyLazy<number>();
+      lazy.setProvider((resolve, reject) => resolve(5));
+      try {
+        lazy.setProvider((resolve, reject) => resolve(6));
+        fail("Should have thrown on a second setProvider call");
+      } catch (e) {
+        expect(e.message).toMatch(/provider has already been set/);
+      }
     });
 
   });
@@ -32,19 +71,37 @@ describe("DependencyInjectionPromise", () => {
   describe(".then", () => {
 
     it("Throws (synchronously) if no provider has been registered, because we expect such registration immediately on import config", () => {
-      let promise = new DependencyInjectionPromise<number>();
+      let promise = new DependencyLazy<number>();
       try {
         promise.then(value => null);
       } catch (e) {
         expect(e.message);
-        expect(e.message).toMatch(/No provider registered for this dependency injection/);
+        expect(e.message).toMatch(/No dependency provider has been set/);
       }
     });
 
-    it("Provides the dependency if it has been resolved", done => {
-      let promise = new DependencyInjectionPromise<number>();
+    it("Provides the dependency again and again", done => {
+      let promise = new DependencyLazy<number>();
+      promise.setProvider(resolve => resolve(9));
+      promise.then(value => expect(value).toBe(9));
       promise.then(value => {
-        expect(value);
+        expect(value).toBe(9);
+        done();
+      });
+
+    });
+
+  });
+
+  describe("Promise.all", () => {
+
+    it("DependencyLazy is compatible, so different injection types can be combined", done => {
+      let promise = new DependencyPromise<boolean>((resolve, reject) => resolve(true));
+      let lazy = new DependencyLazy<number>();
+      lazy.setProvider((resolve, reject) => resolve(42));
+      Promise.all([promise, lazy]).then(([p, l]) => {
+        expect(p).toBeTruthy();
+        expect(l).toBe(42);
         done();
       });
     });
--- src/DependencyInjectionPromise.ts
+++ src/DependencyInjectionPromise.ts
@@ -1,33 +1,48 @@
 
 
-export default class DependencyInjectionPromise<T> extends Promise<T> {
+export interface Dependency<T> extends PromiseLike<T> {
 
-  resolve: (value?: T | PromiseLike<T>) => void;
-
-  reject: (reason?: any) => void;
-
-  constructor() {
-    let captureResolve: (value?: T | PromiseLike<T>) => void;
-    let captureReject: (reason?: any) => void;
-    super((resolve, reject) => {
-      captureResolve = resolve;
-      captureReject = reject;
-    });
-    this.resolve = captureResolve;
-    this.reject = captureReject;
-  }
-
-  providerRegister(executor: (
+  setProvider(executor: (
     resolve: (value?: T | PromiseLike<T>) => void,
     reject: (reason?: any) => void
-  ) => void) {
+  ) => void): void;
 
+}
+
+export class DependencyLazy<T> implements Dependency<T> {
+
+  executor: (
+    resolve: (value?: T | PromiseLike<T>) => void,
+    reject: (reason?: any) => void
+  ) => void = null;
+  promise: Promise<T> = null;
+
+  setProvider(executor: (
+    resolve: (value?: T | PromiseLike<T>) => void,
+    reject: (reason?: any) => void
+  ) => void): void {
+    if (this.executor !== null) throw new Error('A dependency provider has already been set');
+    this.executor = executor;
   }
 
-  /*
-  then(onfulfilled?: (value: T) => T | PromiseLike<T>, onrejected?: (reason: any) => PromiseLike<never>): Promise<T> {
-    return super.then(onfulfilled, onrejected);
+  // https://github.com/Microsoft/TypeScript/blob/v2.8.1/lib/lib.es5.d.ts#L1305
+  then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): PromiseLike<TResult1 | TResult2> {
+    if (this.executor === null) throw new Error('No dependency provider has been set');
+    if (this.promise === null) {
+      this.promise = new Promise<T>(this.executor);
+    }
+    return this.promise.then(onfulfilled, onrejected);
   }
-  */
+
+}
+
+export class DependencyPromise<T> extends Promise<T> implements Dependency<T> {
+
+  setProvider(executor: (
+    resolve: (value?: T | PromiseLike<T>) => void,
+    reject: (reason?: any) => void
+  ) => void): void {
+    throw new Error('This dependency is a regular Promise with no need for a provider');
+  }
 
 }
